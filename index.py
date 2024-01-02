f2aa661c73cd26b6ac1c6acae4737442c160eecb
GeorgeHolm <114369084+GeorgeHolm@users.noreply.github.com>
2024-01-02 17:26:52 -0600

    awesome website

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-import requests
-import flask
+import requests, random
+from flask import Flask, redirect, url_for, render_template
 from bs4 import BeautifulSoup
+
 
 def get_all_links(url):
     content = requests.get(url).content
@@ -9,11 +10,18 @@ def get_all_links(url):
     for link in soup.find_all('a'):
         if link.has_attr('href'):
             href = link['href']
-            if href and not href.startswith('#'):
+            if href and not href.startswith('#') and href.startswith('https://barbend.com'):
                 links.append(href)
     return links
 
-if __name__ == '__main__':
+app = Flask(__name__)
+@app.route("/")
+def home():
     url = 'https://barbend.com'
     links = get_all_links(url)
-    print(links)
+    print(random.choice(links))
+    return render_template("index.html", content=random.choice(links))
+
+if __name__ == "__main__":
+    app.run()
+    
